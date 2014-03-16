0022be1639de5fb365de18b28ba6f308c265d4c0
Marina <marina.hu@laposte.net>
2014-03-16 15:55:41 +0100

    Affichage du fond

---
+++ Walk-Through-Factus/src/com/wtf/screens/PlayScreen.java
@@ -0,0 +1,68 @@
+package com.wtf.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class PlayScreen implements Screen {
+
+	private TiledMap map;
+	private OrthogonalTiledMapRenderer renderer;
+	private OrthographicCamera camera;
+	
+	public PlayScreen() {
+		map = new TmxMapLoader().load("maps/map.tmx");
+		
+		renderer = new OrthogonalTiledMapRenderer(map);
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		
+		renderer.setView(camera);
+		renderer.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void hide() {
+		dispose();
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		map.dispose();
+		renderer.dispose();
+	}
+
+}
